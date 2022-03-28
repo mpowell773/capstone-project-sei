be4e70ae044c278bd21d331f916034448617b9c9
Matt Powell <mpowell171@gmail.com>
2022-03-28 13:38:05 -0400

    created a test room for the map

---
+++ levels/level_data/dungeon_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="scaledTileSet" tilewidth="64" tileheight="64" tilecount="1024" columns="32">
+ <image source="../../assets/graphics/scaled_images/scaledTileSet.png" trans="ff00ff" width="2048" height="2048"/>
+</tileset>
