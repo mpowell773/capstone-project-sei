b9180d1b9fcf7c4f57bd7a7ed65af3bf6650af2a
Matt Powell <mpowell171@gmail.com>
2022-03-28 02:26:30 -0400

    created player.py by copying over tile.py

--- code/player.py
+++ code/player.py
@@ -1,11 +1,11 @@
 import pygame
 from settings import *
 
-class Tile(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         #need to inherit from sprite class via super
         super().__init__()
 
         #get image for tile and add rectangle to it
-        self.image = pygame.image.load('../assets/Graphics/frames/wall_column_mid.png').convert_alpha()
+        self.image = pygame.image.load('../assets/Graphics/frames/elf_m_idle_anim_f0.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
