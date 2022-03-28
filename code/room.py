6b62b0de462a7671e21edeb8fb315623408c9e3b
Matt Powell <mpowell171@gmail.com>
2022-03-28 02:36:20 -0400

    added create_map method and tested nested for loop

--- code/room.py
+++ code/room.py
@@ -1,5 +1,7 @@
 import pygame
 
+from settings import ROOM_1
+
 #class that displays sprites of current room and also handles their interactions
 class Room:
     def __init__(self):
@@ -11,7 +13,17 @@ class Room:
         self.visible_sprites = pygame.sprite.Group
         self.obstacle_sprites = pygame.sprite.Group
 
+        #run create_map method to display sprites
+        self.create_map()
 
+    #method to loop through maps in settings.py to display sprites 
+    def create_map(self):
+        #enumerating to get both row and index
+        for row_index, row in enumerate(ROOM_1):
+            #enumerating individual row to get column and element within row
+            for column_index, column in enumerate(row):
+                pass
+                
 
     def run(self):
         # update/draw game
