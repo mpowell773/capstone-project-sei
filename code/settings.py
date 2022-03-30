3d0e9432dc4bb8179d35bdf98190a8730339ecde
Matt Powell <mpowell171@gmail.com>
2022-03-30 12:41:16 -0400

    added hearts to proper file in assets, defined base settings for UI

--- code/settings.py
+++ code/settings.py
@@ -7,5 +7,27 @@ FPS = 60
 TILESIZE = 64
 NATIVE_SCREEN = pygame.Surface((WIDTH , HEIGHT))
 
-# weapon
+
+
+#UI
+
+HEART_SIZE = 20
+HEART_WIDTH = 200
+HEART_FULL = '../assets/graphics/organized_scaled_tile_set/ui/full.png'
+HEART_HALF = '../assets/graphics/organized_scaled_tile_set/ui/half.png'
+HEART_EMPTY = '../assets/graphics/organized_scaled_tile_set/ui/empty.png'
+
+UI_FONT = '../assets/font/Pixellettersfull-BnJ5.ttf'
+UI_FONT_SIZE = 18
+
+#General colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+#UI Colors
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
+#Weapon
 dagger = {'cooldown' : 100, 'damage': 5, 'graphic': '../assets/graphics/organized_scaled_tile_set/weapons/03_dagger.png' }
