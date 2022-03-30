9157bd14c9731ec4d73c85e52caf933d507956b9
Matt Powell <mpowell171@gmail.com>
2022-03-30 09:51:19 -0400

    added dagger data and also created weapon.py for weapon code

--- code/settings.py
+++ code/settings.py
@@ -6,3 +6,6 @@ HEIGHT = 720
 FPS = 60
 TILESIZE = 64
 NATIVE_SCREEN = pygame.Surface((WIDTH , HEIGHT))
+
+# weapon
+dagger = {'cooldown' : 100, 'damage': 5, 'graphic': '../assets/graphics/organized_scaled_tile_set/weapons/03_dagger.png' }
---
+++ code/weapon.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Dagger(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image
+        self.rect
