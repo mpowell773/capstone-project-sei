65f225eb6abda695f906cb07aff7ed706cc0c043
Matt Powell <mpowell171@gmail.com>
2022-04-01 12:11:19 -0400

    created bow class and some general methods within

---
+++ code/bow.py
@@ -0,0 +1,9 @@
+import pygame
+from settings import *
+
+class Bow:
+    def __init__(self, animation_player):
+        self.animation_player = animation_player
+
+    def arrow(self):
+        pass
