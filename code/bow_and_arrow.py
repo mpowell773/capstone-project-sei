b33dfcf6a034e9c6eae737b8559ade74970b7bc5
Matt Powell <mpowell171@gmail.com>
2022-04-01 13:43:42 -0400

    bow faces only to left or right

--- code/bow_and_arrow.py
+++ code/bow_and_arrow.py
@@ -5,8 +5,10 @@ class Bow_and_Arrow(pygame.sprite.Sprite):
     
     def __init__(self, player, groups):
         super().__init__(groups)
+        #for the bow, only want left or right
         self.player_direction = player.status.split('_')[0]
-        print(self.player_direction)
+        #for arrow, we need up, down, left, or right
+        self.weapon_facing = player.direction_weapon
 
        #graphic of bow
         full_path_bow = f'../assets/graphics/organized_scaled_tile_set/weapons/bow/{self.player_direction}.png'
@@ -16,30 +18,24 @@ class Bow_and_Arrow(pygame.sprite.Sprite):
         full_path_arrow = f'../assets/graphics/organized_scaled_tile_set/weapons/arrow/{self.player_direction}.png'
         self.arrow_image = pygame.image.load(full_path_arrow).convert_alpha()
 
-        #placement of weapon
-        #offsets due to weird hitbox
-        weapon_horizontal_offset = 45
-        weapon_vertical_offset = 75
+        #placement of bow
 
         if  self.player_direction == 'right':
             #place weapon to from it's center left on the player's center right
-            self.rect = self.image.get_rect(midleft = (player.rect.midright[0], player.rect.midright[1] + weapon_horizontal_offset ))
+            self.rect = self.image.get_rect(midleft = (player.rect.midright + pygame.math.Vector2(-25,0)))
         elif self.player_direction == 'left':
-            self.rect = self.image.get_rect(midright = (player.rect.midleft[0], player.rect.midleft[1] + weapon_horizontal_offset))
-        elif self.player_direction == 'up':
-            self.rect = self.image.get_rect(midbottom = (player.rect.midtop[0], player.rect.midtop[1] + weapon_vertical_offset))
-        elif self.player_direction == 'down':
-            self.rect = self.image.get_rect(midtop = (player.rect.midbottom[0], player.rect.midbottom[1]))
+            self.rect = self.image.get_rect(midright = (player.rect.midleft + pygame.math.Vector2(20,0)))
+
 
 
     def arrow(self):
         #get direction of player
         #splitting the _idle/attack off of status
-        if self.player_direction == 'right':
+        if self.weapon_facing == 'right':
             arrow_direction = pygame.math.Vector2(1,0)
-        elif self.player_direction.status.split('_')[0] == 'left':
+        elif self.weapon_facing == 'left':
             arrow_direction = pygame.math.Vector2(-1,0)
-        elif self.player_direction.status.split('_')[0] == 'down':
+        elif self.weapon_facing == 'down':
             arrow_direction = pygame.math.Vector2(0,1)
         else:
             arrow_direction = pygame.math.Vector2(0,-1)
