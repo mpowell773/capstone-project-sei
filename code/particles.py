3fe409f9ad6b2dda1120f16d6be80cf331f5e61d
Matt Powell <mpowell171@gmail.com>
2022-03-31 21:20:36 -0400

    added animate and update method to particles

--- code/particles.py
+++ code/particles.py
@@ -8,4 +8,16 @@ class ParticleEffect(pygame.sprite.Sprite):
         #animation variables
         self.frame_index = 0
         self.animation_speed = 0.15
+        self.frames = animation_frames
         self.image = self.image.get_rect[self.frame_index]
+
+    def animate(self):
+        #quite similar to other animation functions except once the length of the animation if completed, destroy the sprite
+        self.frame_index += self.animation_speed
+        if self.frame >= len(self.frames):
+            self.kill()
+        else:
+            self.image = self.frames[int(self.frame_index)]
+
+    def update(self):
+        self.animate()
