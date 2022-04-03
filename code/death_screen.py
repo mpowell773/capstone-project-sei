5d23455341fc692512606d24fb597a30046e1a44
Matt Powell <mpowell171@gmail.com>
2022-04-03 19:01:54 -0400

    added instructional text for death screen

--- code/death_screen.py
+++ code/death_screen.py
@@ -29,17 +29,21 @@ class Death_Screen:
     def draw_screen(self):
             #create upper text
             death_text = self.font.render("You have died D:", False, TEXT_COLOR)
-            death_rectangle = death_text.get_rect(center = (self.center_width, self.center_height) + pygame.math.Vector2(0, -100))
+            death_rect = death_text.get_rect(center = (self.center_width, self.center_height) + pygame.math.Vector2(0, -100))
 
             #greemie image
             greemie_surface = pygame.image.load('../assets/graphics/organized_scaled_tile_set/entities/player/right_idle/lil-greemie_1.png')
             greemie_surface = pygame.transform.rotozoom(greemie_surface, 90, 1.3)
             greemie_rect = greemie_surface.get_rect(center = (self.center_width, self.center_height) + pygame.math.Vector2(-42, 0))
 
+            #create instruction text
+            instruction_text = self.font.render("Press 'C' to exit out of the game.", False, TEXT_COLOR)
+            instruction_rect = instruction_text.get_rect(center = (self.center_width, self.center_height) + pygame.math.Vector2(0, 100))
 
             #render text and images
-            self.display_surface.blit(death_text, death_rectangle)
+            self.display_surface.blit(death_text, death_rect)
             self.display_surface.blit(greemie_surface, greemie_rect)
+            self.display_surface.blit(instruction_text, instruction_rect)
 
     def run(self):
 
